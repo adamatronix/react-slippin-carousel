61f718c0d8a690048423c5a944669bce6beafe11
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-13 11:28:49 +0800

    draggable Create a test component for testing out the dragging api

---
+++ src/components/DraggableCoreTest/DraggableCoreTest.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const DraggableCoreTest = (props) => {
+
+  const containerStyles = {
+    width: '100%',
+    height: '600px',
+    position: 'relative',
+    background: '#ddd'
+  }
+
+  return (
+    <div style={containerStyles}>
+    </div>
+  )
+}
+
+export default DraggableCoreTest;
