deef7e983c87ccdd179b2ca2cceb15dbdf7efb88
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-14 16:54:31 +0800

    base_carousel Add more items for testing purposes

--- src/components/SlippinCarousel/SlippinCarousel.stories.js
+++ src/components/SlippinCarousel/SlippinCarousel.stories.js
@@ -21,5 +21,17 @@ export const Default = () => (
   <SlippinCarousel>
     <div style={placeholder}></div>
     <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={{...placeholder, ...red}}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
+    <div style={placeholder}></div>
   </SlippinCarousel>
 );
