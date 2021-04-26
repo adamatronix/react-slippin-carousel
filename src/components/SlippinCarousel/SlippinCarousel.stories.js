b30292857d1423d30cdeb459d724511930070748
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-04-26 11:40:12 +0800

    ref-ui Add ability to pass prev and next ui refs

--- src/components/SlippinCarousel/SlippinCarousel.stories.js
+++ src/components/SlippinCarousel/SlippinCarousel.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import SlippinCarousel from './SlippinCarousel';
 import { generatePhotoPlaceholderURL } from 'react-placeholder-image';
 
@@ -22,15 +22,28 @@ const red = {
 
 const imageArray = new Array(10).fill();
 
-export const Default = () => (
-  <SlippinCarousel>
-    { imageArray.map((item, index) => {
-       const image = generatePhotoPlaceholderURL(300, 300);
-        return (
-          <div style={placeholder}>
-            <img src={image}  style={{pointerEvents: 'none'}} />
-          </div>
-        )
-    })}
-  </SlippinCarousel>
-);
+export const Default = () => {
+  
+  const nextRef = useRef();
+  const prevRef = useRef();
+  
+  return (
+    <>
+      <div>
+        <button ref={prevRef}>Prev</button>
+        <button ref={nextRef}>Next</button>
+      </div>
+      <SlippinCarousel prevEl={prevRef} nextEl={nextRef}>
+      { imageArray.map((item, index) => {
+        const image = generatePhotoPlaceholderURL(300, 300);
+          return (
+            <div style={placeholder}>
+              <img src={image}  style={{pointerEvents: 'none'}} />
+            </div>
+          )
+      })}
+    </SlippinCarousel>
+    </>
+  );
+
+}
